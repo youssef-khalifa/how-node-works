76754df2c8d7a962d7a4be14424837a1e49285e5
youssef-khalifa <ykhalifa19921@gmail.com>
2024-02-19 06:28:58 +0200

    third

--- starter/event-loop.js
+++ starter/event-loop.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
-const crypto=require('crypto')
+const crypto = require("crypto");
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 setTimeout(() => console.log("timer 1 finished"), 0);
 setImmediate(() => console.log("immediate 1 finished"));
 fs.readFile("test-file.txt", () => {
@@ -12,7 +14,16 @@ fs.readFile("test-file.txt", () => {
   process.nextTick(() => console.log("process.nextTick "));
 
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log("password encrypted");
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
   });
 });
 console.log("hello from top level code");
