06b853ae342d22b26395c26f8bfd00e2a9d0de75
youssef-khalifa <ykhalifa19921@gmail.com>
2024-02-19 05:59:17 +0200

    second

--- starter/event-loop.js
+++ starter/event-loop.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
+const crypto=require('crypto')
 setTimeout(() => console.log("timer 1 finished"), 0);
-setImmediat(() => console.log("immediate 1 finished"));
-fs.readFile("text-file.txt", () => {
+setImmediate(() => console.log("immediate 1 finished"));
+fs.readFile("test-file.txt", () => {
   console.log("i/o finished");
+  console.log("------------------");
+  setTimeout(() => console.log("timer 2 finished"), 0);
+  setTimeout(() => console.log("timer 3 finished"), 3000);
+  setImmediate(() => console.log("immediate 2 finished"));
+
+  process.nextTick(() => console.log("process.nextTick "));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log("password encrypted");
+  });
 });
 console.log("hello from top level code");
